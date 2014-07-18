4ce6dd6921516cbbfeb84f2e027c7ed6e14c6bfb
Lauri Orgla <lauri.orgla@bigbank.ee>
2014-07-18 16:21:13 +0300

    Some changes

---
+++ src/Hasty2/Cache/FileCacheProvider.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lauri
+ * Date: 7/18/14
+ * Time: 2:38 PM
+ */
+
+namespace Hasty2\Cache;
+
+
+class FileCacheProvider {
+
+} 
---
+++ src/Hasty2/Cache/MemcacheProvider.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lauri
+ * Date: 7/18/14
+ * Time: 2:38 PM
+ */ 
