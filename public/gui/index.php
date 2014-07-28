0dacd7a71ca1cf3277fc67b90d501f17f3227071
Lauri Orgla <TheOrX@hotmail.com>
2014-07-28 20:54:08 +0300

    Commented initialize method, added finally block

---
+++ public/gui/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: prx
+ * Date: 25.07.14
+ * Time: 1:33
+ */ 
--- src/Hasty2/App.php
+++ src/Hasty2/App.php
@@ -21,68 +21,93 @@ class App {
     }
 
     /**
+     * Initializes the whole application.
+     * Sets parameters and path from environment if not defined
+     * Caches request response on demand.
+     *
      * @param null $path
      * @param null $params
      * @param bool $returnOutput
-     *
      * @return string
      */
     public function initialize($path = null, $params = null, $returnOutput = false) {
 
-        Log\Timer::start('request');
+
+        //If path is set, then use from GET variable, otherwise use defined or set to empty string
         if ($path === null && isset($_GET['path'])) {
             $path = $_GET['path'];
         } else {
             $path = '';
         }
 
+        //If params are set then use these, otherwise merge POST and GET.
         if ($params === null && isset($_POST) && isset($_GET)) {
             $params = array_merge($_POST, $_GET);
         } else {
             $params = [];
         }
 
-        $router          = new Router();
-        $responseDTO     = new ResponseDTO([
+
+        //Prepare responseDTO, set queryId and default return code and message.
+        $responseDTO = new ResponseDTO([
             'api'   => new ApiDTO(['queryId' => md5(microtime() . '_' . mt_rand(0, 256000))]),
             'error' => new ErrorDTO(['code' => 200, 'message' => 'ok'])
         ]);
+
+        //Instantiate OutputFormatter
         $outputFormatter = new OutputFormatter();
 
+        //Attempt to get results from router and set response
+        //Also using cache SGClosure for caching response on demand.
         try {
+            //Instantiate router to manage routing.
+            $router = new Router();
 
+            Log\Timer::start('request');
+            //Set result for response.
             $responseDTO->setResult(
+            //Cache result from closure if required.
+            //IF cache ise set to 0 then closure will be execute and result returned instantly.
+
                 Cache::getInstance()->SGClosure(
                     md5($path . print_r($params, true)),
                     function () use ($router, $path, $params) {
 
                         return $router->route($path, $params);
                     },
-                    isset($params['cacheTtl']) ? intval($params['cacheTtl']) : 0
+                    isset($params['cache']) ? intval($params['cache']) : 0
                 )
             );
-
-            $api = $responseDTO->getApi();
-            $api->setCache(Cache::getInstance()->lastStatus());
+            // If cache is set, only then return cache info.
+            if (isset($params['cache'])) {
+                $api = $responseDTO->getApi();
+                $api->setCache(Cache::getInstance()->lastStatus());
+            }
+            //Set responseDTO to formatter so it could be printed out later.
             $outputFormatter->setInput($responseDTO);
 
-        } catch (\Exception $e) {
+        } catch(\Exception $e) {
+            //If exception is thrown, then override ErrorDTO with message and code from thrown exception.
             $responseDTO->setError(
                 new ErrorDTO(['message' => $e->getMessage(), 'code' => $e->getCode(), 'exceptionName' => get_class($e)])
             );
+            //Overwrites the output with new exception.
             $outputFormatter->setInput($responseDTO);
-        }
-        Log\Timer::stop('request');
+        } finally {
+            Log\Timer::stop('request');
 
-        $api            = $responseDTO->getApi();
-        $api->queryTime = Log\Timer::getSeconds('request');
-        $responseDTO->setApi($api);
-        if ($returnOutput) {
-            return $outputFormatter->toJson();
-        } else {
-            print $outputFormatter->toJson(true);
-        }
+            //Update response time after processing the request.
+            $api            = $responseDTO->getApi();
+            $api->queryTime = Log\Timer::getSeconds('request');
+            $responseDTO->setApi($api);
 
+            //Return output
+            if ($returnOutput) {
+                return $outputFormatter->toJson();
+            } else {
+                print $outputFormatter->toJson(true);
+            }
+        }
     }
 
 }
